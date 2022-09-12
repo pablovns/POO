7ee34700ad617087c08a89f98fcfcc3f30ccb184
Pablo Vinicius <viniciuspablo962@gmail.com>
2022-09-11 21:08:03 -0300

    esclarecimento no output

--- Atividade3_5/Testador.java
+++ Atividade3_5/Testador.java
@@ -1,5 +1,6 @@
 public class Testador {
     public static void main(String[] args) {
+        System.out.println("a)");
         Cachorro cachorro = new Cachorro("Catatau", 2);
         cachorro.emitirSom();
 
@@ -8,6 +9,8 @@ public class Testador {
 
         Preguica preguica = new Preguica("Baiano", 5);
         preguica.emitirSom();
+
+        System.out.println("\nb)");
 
         Veterinario.examinar(cachorro);
         Veterinario.examinar(cavalo);
